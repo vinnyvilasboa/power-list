b828cf47b2d72f1ce5d4bb23cef63e4137fe3028
vinnyvilasboa <vinnycesca@gmail.com>
2021-05-19 09:00:13 -0400

    add movie to associations

--- dbTest.js
+++ dbTest.js
@@ -1,7 +1,25 @@
 const db = require('./models');
 
 
-async function addMovie() {
+// async function addMovie() {
+//     const currentUser = await db.user.findByPk(1)
+//     currentUser.then(user => {
+//         db.movie.create({
+//             title: "Get Out",
+//             description: "A young African-American visits his white girlfriend's parents for the weekend, where his simmering uneasiness about their reception of him eventually reaches a boiling point.",
+//             review: 7.7,
+//             user_id: user.id
+//         })
+//             .then(newMovie => {
+//                 console.log(newMovie);
+//             })
+//     })
+
+// }
+// addMovie();
+
+
+async function addNewMovie() {
     const currentUser = await db.user.findByPk(1)
     currentUser.then(user => {
         db.movie.create({
@@ -9,11 +27,15 @@ async function addMovie() {
             description: "A young African-American visits his white girlfriend's parents for the weekend, where his simmering uneasiness about their reception of him eventually reaches a boiling point.",
             review: 7.7,
             user_id: user.id
-      })
+
+        })
             .then(newMovie => {
                 console.log(newMovie);
-
+            })
+            .catch(err => {
+                console.log(err);
             })
     })
-
 }
+
+addNewMovie();
