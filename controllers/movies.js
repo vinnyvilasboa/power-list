bcec041f2af9e74e78013f0380c6c87f905b6b4c
vinnyvilasboa <vinnycesca@gmail.com>
2021-05-20 21:11:03 -0400

    new page route ejs

--- controllers/movies.js
+++ controllers/movies.js
@@ -16,16 +16,29 @@ router.get('/results', async (req, res) => {
     res.render('movies/results', { movieResults: results.data.Search ? results.data.Search : [] });
 
 })
-
+router.get('/new', (req, res) => {
+    res.render('movies/new')
+})
 router.post('/new', async (req, res) => {
-    const createMovie = await db.movie.create(req.body)
-    res.redirect('/movies')
-    console.log(createMovie)
+    const createMovie = await db.movie.findOrCreate({
+        where: {title: req.body.title},
+        default: {title: req.body}
+
+    })
+    res.redirect('new')
+    console.log(req.body)
 })
 
 module.exports = router;
 
+/*
+router.post('/', async (req,res) => {
+  const [category, created] = await db.category.findOrCreate({
+    where: {name: req.body.category},
+    defaults: {name: req.body.category}
+  })
 
+*/
 // router.post('/', function(req, res) {
 //     // TODO: Get form data and add a new record to DB
 //     const createMovie = db.movie.create(req.body)
