fbc7ffd9d674275f76c4a0128e5ec2500d95581c
vinnyvilasboa <vinnycesca@gmail.com>
2021-05-22 12:09:22 -0400

    mvp- now to work on css

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,8 +2,22 @@ const express = require('express');
 const router = express.Router();
 const axios = require("axios");
 const db = require('../models');
+const { route } = require('./auth');
 
 const APIKey = process.env.API_KEY;
+
+router.get('/', (req, res)=>{
+    db.movie.findAll()
+    .then((foundMovies) => {
+        console.log("here is found movie")
+        console.log(foundMovies)
+        res.render('movies/index', {
+            allMovies: foundMovies
+        })
+    })
+})
+
+
 
 router.get('/search', (req, res) => {
     res.render('movies/search')
@@ -24,23 +38,36 @@ router.post('/new', async (req, res) => {
         where: {title: req.body.title},
         defaults: {review: req.body.review, description: req.body.description, userId: req.user?req.user.id:1}
     })
-    res.redirect('/movies/new')
+    res.redirect('/movies')
     console.log(req.body)
 })
 
+router.put('/update', (req, res) => {
+    console.log(req.body.updateDescription)
+    console.log("here is updateDescription")
+    console.log(req.body.updateReview)
+    console.log("here is updateReview")
+    console.log(req.body.movieId)
+    db.movie.update(
+        {
+            description: req.body.updateDescription,
+            review: req.body.updateReview
+        },
+        { where: {id: req.body.movieId} }
+    ).then(updatedMovie => {
+        console.log("here is updated movie")
+        console.log(updatedMovie)
+        res.redirect('/movies')
+    })
+})
+
+router.delete('/delete', (req, res)=> {
+    db.movie.destroy(
+        { where: {id: req.body.delete} }
+    ).then(deletedMovie => {
+        console.log("movie successfully deleted")
+        res.redirect('/movies')
+    })
+})
 module.exports = router;
 
-/*
-router.post('/', async (req,res) => {
-  const [category, created] = await db.category.findOrCreate({
-    where: {name: req.body.category},
-    defaults: {name: req.body.category}
-  })
-
-*/
-// router.post('/', function(req, res) {
-//     // TODO: Get form data and add a new record to DB
-//     const createMovie = db.movie.create(req.body)
-//       res.redirect('/movies')
-//     })
-//   });
